b2a8fd8217f255ebece55e4896724bd1e136d9a2
leonhming <leon.min@ximalaya.com>
2017-10-23 11:17:29 +0800

    添加

---
+++ XAndroidFrawork/xfragwork/src/android/loser/xfragwork/utils/AppInfo.java
@@ -0,0 +1,48 @@
+package android.loser.xfragwork.utils;
+
+import android.content.Context;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+
+/**
+ * @version V2.7.0
+ * @Project: CMN-Android
+ * @Filename:com.asc.ud.utils
+ * @Desciption:
+ * @Author: sunbo
+ * @Date: 2017/8/22  9:38
+ * @Copyright: 2017 AgileSC, Inc. China All rights reserved.
+ * Modification History
+ * Date				Author	Version 	Desciption
+ */
+
+public class AppInfo {
+    public static String getVersionName(Context context) {
+        //获取当前版本号
+        PackageInfo pi = null;//getPackageName()是你当前类的包名，0代表是获取版本信息
+        try {
+            pi = context.getApplicationContext().getPackageManager().getPackageInfo(context.getApplicationContext().getPackageName(), 0);
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+        //版本名称
+        String name = pi.versionName;
+        //版本号
+        int code = pi.versionCode;
+        return name;
+    }
+
+    public static int getVersionCode(Context context) {
+        //获取当前版本号
+        PackageInfo pi = null;//getPackageName()是你当前类的包名，0代表是获取版本信息
+        try {
+            pi = context.getApplicationContext().getPackageManager().getPackageInfo(context.getApplicationContext().getPackageName(), 0);
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+        //版本号
+        int code = pi.versionCode;
+        return code;
+    }
+
+}
---
+++ XAndroidFrawork/xfragwork/src/android/loser/xfragwork/utils/PermissionsUtils.java
@@ -0,0 +1,43 @@
+package android.loser.xfragwork.utils;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.support.v4.app.ActivityCompat;
+
+import com.loser.R;
+import com.loser.photograph.base.BaseActivity;
+import com.tbruyelle.rxpermissions2.RxPermissions;
+
+/**
+ * @version V2.7.0
+ * @Project: CMN-Android
+ * @Filename:com.asc.cmn.utils
+ * @Desciption:
+ * @Author: sunbo
+ * @Date: 2017/8/15  15:18
+ * @Copyright: 2017 AgileSC, Inc. China All rights reserved.
+ * Modification History
+ * Date				Author	Version 	Desciption
+ */
+
+public class PermissionsUtils {
+    public static void callPhone(BaseActivity activity, String phoneNumber) {
+        RxPermissions rxPermissions = new RxPermissions(activity);
+        rxPermissions.request(Manifest.permission.CALL_PHONE)
+                .subscribe(granted -> {
+                    if (granted) {
+                        if (ActivityCompat.checkSelfPermission(activity, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+                            return;
+                        }
+                        Intent intent = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:" + phoneNumber));
+                        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                        activity.startActivity(intent);
+                    } else {
+                        //没有权限
+                        ToastUtil.showToast(activity, activity.getString(R.string.call_phone));
+                    }
+                });
+    }
+}
